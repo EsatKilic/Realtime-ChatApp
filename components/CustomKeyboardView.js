619ae3dd9367ce76178db482880b8f7c1cd59666
Murat Esat Kılıç <127654031+EsatKilic@users.noreply.github.com>
2024-10-11 16:50:06 +0300

    [DEV][esat.kilic][add some components and regulate _layout.js and home.js]

--- components/CustomKeyboardView.js
+++ components/CustomKeyboardView.js
@@ -1,19 +1,30 @@
-import { View, Text, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
+import { View, Text, KeyboardAvoidingView, ScrollView, Platform } from 'react-native'
 import React from 'react'
 
-const ios= Platform.OS=='ios';
-export default function CustomKeyboardView({children}) {
+const ios = Platform.OS == 'ios';
+export default function CustomKeyboardView({children, inChat}) {
+    let kavConfig = {};
+    let scrollViewConfig = {};
+    if(inChat){
+        kavConfig = {keyboardVerticalOffset: 90};
+        scrollViewConfig = {contentContainerStyle: {flex: 1}};
+    }
   return (
-    <KeyboardAvoidingView behavior={ios? 'padding' : 'height'}
-    style={{flex:1}}>
-        <ScrollView style ={{flex:1}}
-        bounces={false}
-        showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+        behavior={ios? 'padding': 'height'}
+        style={{flex: 1}}
+        {...kavConfig}
+    >
+        <ScrollView
+            style={{flex: 1}}
+            bounces={false}
+            showsVerticalScrollIndicator={false}
+            {...scrollViewConfig}
         >
-            {children}
-
+            {
+                children
+            }
         </ScrollView>
-
-     </KeyboardAvoidingView>
+    </KeyboardAvoidingView>
   )
 }
