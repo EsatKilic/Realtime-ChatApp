98d2f33539502260194b4366bc924b01e6dc1f45
Murat Esat Kılıç <127654031+EsatKilic@users.noreply.github.com>
2024-10-14 23:07:52 +0300

    [FIX][esat.kilic][fix signIn and signUp]

--- app/signIn.js
+++ app/signIn.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity,Image, Pressable, Alert  } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity, Pressable, Alert } from 'react-native'
 import React, { useRef, useState } from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { StatusBar } from 'expo-status-bar';
@@ -8,95 +8,99 @@ import Loading from '../components/Loading';
 import CustomKeyboardView from '../components/CustomKeyboardView';
 import { useAuth } from '../context/authContext';
 
-
-
 export default function SignIn() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  const {login}= useAuth;
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const {login} = useAuth();
 
-  const emailRef = useRef("");
-  const passworfRef = useRef("");
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
 
-  const handleLogin = async ()=>{
-    if(!emailRef.current || !passworfRef.current){
-      Alert.alert('Sign In', "Please fill all the fields");
-      return;
+    const handleLogin = async ()=>{
+        if(!emailRef.current || !passwordRef.current){
+            Alert.alert('Sign In', "Please fill all the fields!");
+            return;
+        }
+
+        setLoading(true);
+        const response = await login(emailRef.current, passwordRef.current);
+        setLoading(false);
+        console.log('sign in reposnse: ', response);
+        if(!response.success){
+            Alert.alert('Sign In', response.msg);
+        }
     }
-
-    setLoading(true);
-    const response = await login(emailRef.current,passworfRef.current);
-    setLoading(false);
-    console.log('sign in response', response);
-    if(!response.success){
-      Alert.alert('Sign In', response.msg);
-    }
-
-  }
-
   return (
     <CustomKeyboardView>
-      <StatusBar style="dark"/>
-      <View style={{paddingTop:(8),paddingHorizontal:wp(5)}} className="flex-1 gap-12">
+      <StatusBar style="dark" />
+      <View style={{paddingTop: hp(8), paddingHorizontal: wp(5)}} className="flex-1 gap-12">
+        {/* signIn image */}
         <View className="items-center">
-          <Image style={{height: hp(25)}} resizeMode='contain' source ={require('../assets/images/login.png')}/>
+            <Image style={{height: hp(25)}} resizeMode='contain' source={require('../assets/images/login.png')} />
         </View>
 
+
         <View className="gap-10">
-          <Text style={{fontSize:(4)}} className="font-bold tracking-wider text-center text-neutral-800" >signIn</Text>
-          <View className="gap-4">
-          <View style={{height:hp(7)}} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
-            <Octicons name="mail" size={hp(2.7)} color="gray" />
-            <TextInput
-            onChangeText={value=>emailRef.current=value}
-            style={{fontSize: hp(2)}}
-            className="flex-1 font-semibold text-neutral-700"
-            placeholder='Email Adress'
-            placeholderTextColor={gray}
-            />
-          </View>
-          <View className="gap-3">
-          <View style={{height:hp(7)}} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
-            <Octicons name="lock" size={hp(2.7)} color="gray" />
-            <TextInput
-            onChangeText={value=>passworfRef.current=value}
-            style={{fontSize: hp(2)}}
-            className="flex-1 font-semibold text-neutral-700"
-            placeholder='Password'
-            secureTextEntry
-            placeholderTextColor={gray}
-            />
-            </View>
-            <Text style={{fontSize:hp(1.8)}} className="font-semibold text-righttext-neutral-500">Forgot Password?</Text>
-          </View>
-          <View>
-            {
-              loading? (
-                <View className="flex-row justify-center">
-                  <Loading size={hp(6.5)} />
+            <Text style={{fontSize: hp(4)}} className="font-bold tracking-wider text-center text-neutral-800">Sign In</Text>
+            {/* inputs */}
+            <View className="gap-4">
+                <View style={{height: hp(7)}} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
+                    <Octicons name="mail" size={hp(2.7)} color="gray" />
+                    <TextInput
+                        onChangeText={value=> emailRef.current=value}
+                        style={{fontSize: hp(2)}}
+                        className="flex-1 font-semibold text-neutral-700"
+                        placeholder='Email address'
+                        placeholderTextColor={'gray'}
+                    />
                 </View>
-              ):(
-                <TouchableOpacity onPress={handleLogin} style={{fontSize: hp(6.5)}} className="bg-indigo-500 rounded-xl justify-center items-center">
-                <Text style={{fontSize:hp(2.7)}} className="text-white font-bold tracking-wider">
-                  Sign In
-                </Text>
-              </TouchableOpacity>
-              )
-            }
-          </View>
-           
+                <View className="gap-3">
+                    <View style={{height: hp(7)}} className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
+                        <Octicons name="lock" size={hp(2.7)} color="gray" />
+                        <TextInput
+                            onChangeText={value=> passwordRef.current=value}
+                            style={{fontSize: hp(2)}}
+                            className="flex-1 font-semibold text-neutral-700"
+                            placeholder='Password'
+                            secureTextEntry
+                            placeholderTextColor={'gray'}
+                        />
+                    </View>
+                    <Text style={{fontSize: hp(1.8)}} className="font-semibold text-right text-neutral-500">Forgot password?</Text>
+                </View>
 
-            <View className="flex-row justify-center">
-              <Text style={{fontSize:hp(1.8)}} className="font-semibold text-neutral-500">Don' t Have an Account? </Text>
-              <Pressable onPress={()=> router.push('signUp')}>
+                {/* submit button */}
 
-              <Text style={{fontSize:hp(1.8)}} className="font-bold text-indigo-500"> Sign Up</Text>
-              </Pressable>
-              
+                <View>
+                    {
+                        loading? (
+                            <View className="flex-row justify-center">
+                                <Loading size={hp(6.5)} />
+                            </View>
+                        ):(
+                            <TouchableOpacity onPress={handleLogin} style={{height: 50, backgroundColor: '#6366F1', borderRadius: 10, justifyContent: 'center', alignItems: 'center', zIndex: 1}}>
+                                <Text style={{fontSize: hp(2.7)}} className="text-white font-bold tracking-wider">
+                                    Sign In
+                                </Text>
+                            </TouchableOpacity>
+                        )
+                    }
+                </View>
 
+                
+
+                {/* sign up text */}
+
+                <View className="flex-row justify-center">
+                    <Text style={{fontSize: hp(1.8)}} className="font-semibold text-neutral-500">Don't have an account? </Text>
+                    <Pressable onPress={()=> router.push('signUp')}>
+                        <Text style={{fontSize: hp(1.8)}} className="font-bold text-indigo-500">Sign Up</Text>
+                    </Pressable>
+                    
+                </View>
+                
             </View>
-
-          </View>
+            
         </View>
       </View>
     </CustomKeyboardView>
