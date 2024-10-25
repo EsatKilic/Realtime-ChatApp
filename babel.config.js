1b082caf12fe7a7f7fd7c772c3fd74186d59a198
Murat Esat Kılıç <127654031+EsatKilic@users.noreply.github.com>
2024-10-25 18:35:49 +0300

    android files eas reg

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      "@babel/plugin-proposal-export-namespace-from",
+    ],
+    plugins: [
+      // Required for expo-router
+      '@babel/plugin-proposal-export-namespace-from',
       "react-native-reanimated/plugin",
     ],
   };
